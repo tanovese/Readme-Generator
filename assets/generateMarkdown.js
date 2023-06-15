e1a92fb8d519b65c1a9e7e0476dbeeff97cb846e
tanovese <82059748+tanovese@users.noreply.github.com>
2023-06-15 17:29:18 -0400

    Added descriptive comments to code

--- assets/generateMarkdown.js
+++ assets/generateMarkdown.js
@@ -1,18 +1,9 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// renderLicenseBadge returns a license badge from the shields.io link.
 function renderLicenseBadge(license) {
   return `https://img.shields.io/badge/license-${license}-blue.svg`;
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// Function to generateMarkdown file.
 function generateMarkdown(data) {
   return `# ${data.title}
   ![License:](${renderLicenseBadge(data.license)})
@@ -43,4 +34,5 @@ function generateMarkdown(data) {
 `;
 }
 
+// Exports the generateMarkdown.js file
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-// TODO: Include packages needed for this application
+// The following are the packages needed for the application. Also, I am referencing the generateMarkdown function
+// in the generateMarkdown const so that I can pass data below to the function
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./assets/generateMarkdown');
 
-// TODO: Create an array of questions for user input
+// The following are the questions that the user will be promped to respond to in the terminal
 const questions = [{
   type: 'input',
   message: 'Please enter your project title',
@@ -50,21 +51,23 @@ const questions = [{
   message: 'Please enter your email address and instructions on how to reach you',
   name: 'email',
 }]
-
-// TODO: Create a function to write README file
+// The writetoFile function takes in two parameters, fileName and data.
+// the fs.writeFile then creates the file once we fill in those parameters in the
+// init function below
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (err) =>
   err ? console.log(err) : console.log('Success!')
 )};
 
-// TODO: Create a function to initialize app
+// The init function initializes the app using inquirer and .prompt
 function init() {
   inquirer
   .prompt(questions)
   .then((response) => {
 console.log(response);
 console.log(generateMarkdown(response));
-// call the writeToFile function and give it the filename and generateMarkdown(response)
+// writetoFile uses the given filename and the generateMarkdown function with the response
+// which creates the md file.
 writeToFile(`project-readme.md`, generateMarkdown(response))
 });
 }
